18f34673422bb2f5526f2270376c7e483c9bd693
akrherz <akrherz@iastate.edu>
2014-01-14 12:14:39 -0600

    add two includes used by upstream

--- include/props.php
+++ include/props.php
@@ -1,7 +1,10 @@
 <?php 
 // Fake values
 $config = Array(
-		
+	"punjab_route" => "xmpp:nwschat-openfire:5222",
+	"bosh_service" => "/bosh/",
+	"xmpp_domain" => "laptop.local",
+	"nwschatadmin" => "akrherz@localhost",	
 );
 
 ?>
