1b45772b27bfcee427817bc356b647c39e353520
akrherz <akrherz@iastate.edu>
2014-02-19 13:41:16 -0600

    add crude 'deployment' that copies live over to nwschat

---
+++ deployment/nwschat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script does what is necessary to copy relative bits out of this
+# project and into NWSChat's RCMS
+
+NWSCHAT="/home/nwschat/public_html/live/"
+LIVE="/home/akrherz/projects/live/public_html/live/"
+
+rsync -av $LIVE $NWSCHAT
