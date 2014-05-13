088c259180162a9efcc1863f35bceb532d92d7f2
akrherz <akrherz@iastate.edu>
2014-05-13 11:55:08 -0500

    update deployment script

--- deployment/nwschat.sh
+++ deployment/nwschat.sh
@@ -2,7 +2,8 @@
 # This script does what is necessary to copy relative bits out of this
 # project and into NWSChat's RCMS
 
-NWSCHAT="/home/nwschat/public_html/live/"
+NWSCHAT="/home/nwschat_2.1/public_html/live/"
 LIVE="/home/akrherz/projects/live/public_html/live/"
 
 rsync -av $LIVE $NWSCHAT
+rm -f $NWSCHAT/.gitignore
