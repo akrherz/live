15676c6d673799709147be633590be7672e5b930
akrherz <akrherz@iastate.edu>
2014-05-19 14:39:00 -0500

    backport forEach for IE8 too

--- public_html/live/js/overrides.js
+++ public_html/live/js/overrides.js
@@ -43,6 +43,32 @@ if (!Object.keys) {
   }());
 }
 
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Compatibility
+ */
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisArg */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        fun.call(thisArg, t[i], i, t);
+    }
+  };
+}
+
 /* 
  * Need to be able to have filters that can be reapplied
  * http://www.sencha.com/forum/showthread.php?76029-DISCUSS-Persistent-store-filter
