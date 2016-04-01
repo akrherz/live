72d8577975b1060bdc238c15522979aeae7f6c02
akrherz <akrherz@iastate.edu>
2016-04-01 13:05:26 -0500

    update deployment
    Updated to latest Google Closure Compiler as well

--- deployment/nwschat.sh
+++ deployment/nwschat.sh
@@ -2,7 +2,7 @@
 # This script does what is necessary to copy relative bits out of this
 # project and into NWSChat's RCMS
 
-NWSCHAT="/home/nwschat_2.1/public_html/live/"
+NWSCHAT="/home/akrherz/projects/nwschat/public_html/live/"
 LIVE="/home/akrherz/projects/live/public_html/live/"
 
 rsync -av $LIVE $NWSCHAT
--- deployment/weather_im.sh
+++ deployment/weather_im.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # This script does what is necessary to copy relative bits out of this
-# project and into the tree for the weather.im service
+# project and into the tree for the weather.im service, we run this on iem12
 
 DEST="/mesonet/www/apps/weather.im/html/live/"
 LIVE="/mesonet/www/apps/live/public_html/live/"
 
 rsync -av $LIVE $DEST
+cd $DEST
+
+MACHINES="iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
+for MACH in $MACHINES
+do
+rsync -av --delete . ${MACH}:`pwd`
+done
