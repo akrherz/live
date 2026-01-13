25364e62d55b8a38461725edaf2e35ab295eab89
akrherz <akrherz@iastate.edu>
2026-01-13 11:57:39 -0600

    🎨 Address some lint

--- src/events/event-handlers.js
+++ src/events/event-handlers.js
@@ -3,136 +3,138 @@
  * MsgBus events, sound system, chat events
  */
 
-import { $iq, $pres } from 'strophe.js';
+import { $iq, $pres } from "strophe.js";
 
 export function UTCStringToDate(dtStr, format) {
     const dt = Date.parseDate(dtStr, format);
-    if (dt == undefined)
-        return ''; // or whatever you want to do
-    if (typeof dt.fromUTC === 'function') {
+    if (dt == undefined) return ""; // or whatever you want to do
+    if (typeof dt.fromUTC === "function") {
         return dt.fromUTC();
     }
     return dt;
 }
 
 Application.MsgBus = new Ext.util.Observable();
-Application.MsgBus.addEvents('message');
-Application.MsgBus.addEvents('loggedin');
-Application.MsgBus.addEvents('loggedout');
+Application.MsgBus.addEvents("message");
+Application.MsgBus.addEvents("loggedin");
+Application.MsgBus.addEvents("loggedout");
 
 // Audio cache for reusing Audio objects
 const audioCache = {};
 
-Application.playSound = function(sidx) {
+Application.playSound = function (sidx) {
     // Check if audio is muted
     if (Application.audioMuted) {
         return;
     }
-    
-    const idx = Application.SoundStore.find('id', sidx);
+
+    const idx = Application.SoundStore.find("id", sidx);
     if (idx == -1) {
         Application.log("Could not find sound: " + sidx);
         return;
     }
-    
+
     const record = Application.SoundStore.getAt(idx);
     const url = record.get("src");
-    
+
     // Create or reuse audio element
     if (!audioCache[sidx]) {
         audioCache[sidx] = new Audio(url);
     }
-    
+
     const audio = audioCache[sidx];
-    audio.volume = Application.getPreference('volume', 100) / 100;
-    
+    audio.volume = Application.getPreference("volume", 100) / 100;
+
     // Reset to beginning if already playing
     audio.currentTime = 0;
-    audio.play().catch(function(err) {
-        console.error('Error playing sound:', err);
+    audio.play().catch(function (err) {
+        console.error("Error playing sound:", err);
     });
 };
 
-Application.MsgBus.on('soundevent', function(sevent) {
-
-    const enable = Application.getPreference("sound::" + sevent + "::enabled", 'true');
-    if (enable == 'false') {
+Application.MsgBus.on("soundevent", function (sevent) {
+    const enable = Application.getPreference(
+        "sound::" + sevent + "::enabled",
+        "true"
+    );
+    if (enable == "false") {
         return;
     }
-    const sidx = Application.getPreference("sound::" + sevent + "::sound", 'default');
+    const sidx = Application.getPreference(
+        "sound::" + sevent + "::sound",
+        "default"
+    );
     Application.playSound(sidx);
-
 });
 
-Application.MsgBus.on('loggingout', function() {
+Application.MsgBus.on("loggingout", function () {
     /* Remove chatrooms from view */
-    Ext.getCmp("chatpanel").items.each(function(panel) {
-                if (panel.chatType == "groupchat") {
-                    //Ext.getCmp('chatpanel').remove(panel);
-                    panel.clearRoom();
-                }
-            });
+    Ext.getCmp("chatpanel").items.each(function (panel) {
+        if (panel.chatType == "groupchat") {
+            //Ext.getCmp('chatpanel').remove(panel);
+            panel.clearRoom();
+        }
+    });
     /* Remove buddies */
-    Ext.getCmp('buddies').root.removeAll();
+    Ext.getCmp("buddies").root.removeAll();
     /* Remove bookmarks */
-    Ext.getCmp('bookmarks').root.suspendEvents(false);
-    Ext.getCmp('bookmarks').root.removeAll();
-    Ext.getCmp('bookmarks').root.resumeEvents();
-    Ext.getCmp('bookmarks').root.initalLoad = false;
+    Ext.getCmp("bookmarks").root.suspendEvents(false);
+    Ext.getCmp("bookmarks").root.removeAll();
+    Ext.getCmp("bookmarks").root.resumeEvents();
+    Ext.getCmp("bookmarks").root.initalLoad = false;
     /* Remove chatrooms */
-    Ext.getCmp('chatrooms').root.removeAll();
-
+    Ext.getCmp("chatrooms").root.removeAll();
 });
 
-Application.MsgBus.on('loggedout', function() {
-    Ext.getCmp('loginwindow').show();
+Application.MsgBus.on("loggedout", function () {
+    Ext.getCmp("loginwindow").show();
 });
 
-Application.MsgBus.on('loggedin', function() {
+Application.MsgBus.on("loggedin", function () {
+    Ext.getCmp("loginwindow").hide();
 
-            Ext.getCmp('loginwindow').hide();
+    Application.XMPPConn.send(
+        $iq({
+            type: "get",
+            id: "fetchrooms",
+            to: "conference." + Application.XMPPHOST,
+        }).c("query", {
+            xmlns: "http://jabber.org/protocol/disco#items",
+        })
+    );
+});
 
-            Application.XMPPConn.send($iq({
-                        type : 'get',
-                        id : 'fetchrooms',
-                        to : 'conference.' + Application.XMPPHOST
-                    }).c('query', {
-                        xmlns : 'http://jabber.org/protocol/disco#items'
-                    }));
-
+Application.MsgBus.on("joinchat", function (room, handle, anonymous) {
+    if (handle == null || handle == "") {
+        handle = Application.USERNAME;
+    }
+    let mcp = Ext.getCmp("chatpanel").getMUC(room);
+    if (mcp == null) {
+        Application.log("Creating chatroom:" + room);
+        mcp = Ext.getCmp("chatpanel").addMUC(room, handle, anonymous);
+        // Ext.getCmp("chatpanel").setActiveTab(mcp);
+        /* Initial Presence */
+        const p = $pres({
+            to: room + "/" + handle,
         });
-
-Application.MsgBus.on('joinchat', function(room, handle, anonymous) {
-            if (handle == null || handle == "") {
-                handle = Application.USERNAME;
+        if (anonymous) {
+            p.c("x", {
+                xmlns: "http://jabber.org/protocol/muc#user",
+            })
+                .c("item")
+                .c("role", "visitor");
+        }
+        Application.XMPPConn.send(p);
+        mcp.on("destroy", function () {
+            if (Application.XMPPConn.authenticated && this.joinedChat) {
+                Application.XMPPConn.send(
+                    $pres({
+                        type: "unavailable",
+                        to: room + "/" + handle,
+                    })
+                );
             }
-            let mcp = Ext.getCmp("chatpanel").getMUC(room);
-            if (mcp == null) {
-                
-                Application.log("Creating chatroom:" + room);
-                mcp = Ext.getCmp("chatpanel").addMUC(room, handle, anonymous);
-                // Ext.getCmp("chatpanel").setActiveTab(mcp);
-                /* Initial Presence */
-                const p = $pres({
-                            to : room + '/' + handle
-                        });
-                if (anonymous) {
-                    p.c('x', {
-                                'xmlns' : 'http://jabber.org/protocol/muc#user'
-                            }).c('item').c('role', 'visitor');
-                }
-                Application.XMPPConn.send(p);
-                mcp.on('destroy', function() {
-                            if (Application.XMPPConn.authenticated &&
-                                    this.joinedChat) {
-                                Application.XMPPConn.send($pres({
-                                            type : 'unavailable',
-                                            to : room + '/' + handle
-                                        }));
-                            }
-                });
-
-            }
-            Ext.getCmp("chatpanel").setActiveTab(mcp);
-
         });
+    }
+    Ext.getCmp("chatpanel").setActiveTab(mcp);
+});
--- src/ui/core-components.js
+++ src/ui/core-components.js
@@ -3,146 +3,165 @@
  * DebugWindow, LiveViewport, MapLegend
  */
 
-import { createLoginPanel } from '../auth/LoginPanel.js';
+import { createLoginPanel } from "../auth/LoginPanel.js";
 
-Application.msgtpl = new Ext.XTemplate('<p>{date:date("g:i:s A")} :: {msg}</p>');
+Application.msgtpl = new Ext.XTemplate(
+    '<p>{date:date("g:i:s A")} :: {msg}</p>'
+);
 
 Application.DebugWindow = Ext.extend(Ext.Window, {
-    initComponent : function(){
-        this.items = [{
-            xtype: 'panel',
-            title : 'Debug Log',
+    initComponent: function () {
+        this.items = [
+            {
+                xtype: "panel",
+                title: "Debug Log",
 
-            html : "<p>Browser CodeName: " + navigator.appCodeName + "</p>"
-            + "<p>Browser Name: " + navigator.appName + "</p>"
-            + "<p>Browser Version: " + navigator.appVersion + "</p>"
-            + "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>"
-            + "<p>Platform: " + navigator.platform + "</p>"
-            + "<p>User-agent header: " + navigator.userAgent + "</p>",
-            autoScroll : true
-        }];
-    
-        this.tbar = [{
-            text : 'Clear Log',
-            icon : 'icons/close.png',
-            handler : function(){
-                this.items.items[0].update("");
+                html:
+                    "<p>Browser CodeName: " +
+                    navigator.appCodeName +
+                    "</p>" +
+                    "<p>Browser Name: " +
+                    navigator.appName +
+                    "</p>" +
+                    "<p>Browser Version: " +
+                    navigator.appVersion +
+                    "</p>" +
+                    "<p>Cookies Enabled: " +
+                    navigator.cookieEnabled +
+                    "</p>" +
+                    "<p>Platform: " +
+                    navigator.platform +
+                    "</p>" +
+                    "<p>User-agent header: " +
+                    navigator.userAgent +
+                    "</p>",
+                autoScroll: true,
             },
-            scope: this
-        }];    
-        
-        var config = {
-                width : 600,
-                height : 300,
-                title : 'Debug Window',
-                closeAction : 'hide',
-                hidden : true,
-                autoScroll : true,
-                layout : 'fit'
+        ];
+
+        this.tbar = [
+            {
+                text: "Clear Log",
+                icon: "icons/close.png",
+                handler: function () {
+                    this.items.items[0].update("");
+                },
+                scope: this,
+            },
+        ];
+
+        const config = {
+            width: 600,
+            height: 300,
+            title: "Debug Window",
+            closeAction: "hide",
+            hidden: true,
+            autoScroll: true,
+            layout: "fit",
         };
         Ext.apply(this, Ext.apply(this.initialConfig, config));
 
-        Application.DebugWindow.superclass.initComponent.apply(this,
-                                        arguments);
+        Application.DebugWindow.superclass.initComponent.apply(this, arguments);
     }, // End of initComponent
-    addMessage : function(msg){
-        Application.msgtpl.append(
-                this.items.items[0].body, {
-                        msg : msg,
-                        date : new Date()
-                });
-    } // End of addMessage
+    addMessage: function (msg) {
+        Application.msgtpl.append(this.items.items[0].body, {
+            msg: msg,
+            date: new Date(),
+        });
+    }, // End of addMessage
 });
 
-
 Application.LiveViewport = Ext.extend(Ext.Viewport, {
-    initComponent : function() {
-        var mp = {
-                xtype : 'panel',
-                region : 'north',
-                height : 10,
-                hidden : true,
-                title : 'Map Disabled by URL'
+    initComponent: function () {
+        let mp = {
+            xtype: "panel",
+            region: "north",
+            height: 10,
+            hidden: true,
+            title: "Map Disabled by URL",
         };
-        if (this.initialConfig.enableMap){
+        if (this.initialConfig.enableMap) {
             mp = {
-                    xtype : 'panel',
-                    layout : 'border',
-                    region : 'north',
-                    collapsible : true,
-                    title : 'Map Panel',
-                    height : 300,
-                    split : true,
-                    items : [Application.MapPanel,
-                             Application.LayerTree]
+                xtype: "panel",
+                layout: "border",
+                region: "north",
+                collapsible: true,
+                title: "Map Panel",
+                height: 300,
+                split: true,
+                items: [Application.MapPanel, Application.LayerTree],
             };
         }
-        this.items = [Application.Control, {
-            xtype : 'panel',
-            region : 'center',
-            layout : 'border',
-            items : [mp, new Application.ChatTabPanel({
-                id : 'chatpanel',
-                region : 'center',
-                split : true
-            })]
-        }];
-        var config = {
-                layout: 'border'
+        this.items = [
+            Application.Control,
+            {
+                xtype: "panel",
+                region: "center",
+                layout: "border",
+                items: [
+                    mp,
+                    new Application.ChatTabPanel({
+                        id: "chatpanel",
+                        region: "center",
+                        split: true,
+                    }),
+                ],
+            },
+        ];
+        const config = {
+            layout: "border",
         };
         Ext.apply(this, Ext.apply(this.initialConfig, config));
         Application.LiveViewport.superclass.initComponent.call(this);
         this.doStuff();
-    }, doStuff: function(){
-        var mp = Ext.getCmp("map");
+    },
+    doStuff: function () {
+        const mp = Ext.getCmp("map");
         if (mp && mp.map && mp.map.events) {
-          mp.map.events.register("changelayer", null, function(evt){
-            var myobj = {lstring: ''};
-            Application.layerstore.data.each(function(record) {
-              var layer = record.getLayer();
-              if (layer.getVisibility()) {
-                this.lstring += "||"+ layer.name;
-              }
-            }, myobj);
-            Application.setPreference("layers", myobj.lstring);
-          });
-          Ext.TaskMgr.start(Application.MapTask);
+            mp.map.events.register("changelayer", null, function () {
+                const myobj = { lstring: "" };
+                Application.layerstore.data.each(function (record) {
+                    const layer = record.getLayer();
+                    if (layer.getVisibility()) {
+                        this.lstring += "||" + layer.name;
+                    }
+                }, myobj);
+                Application.setPreference("layers", myobj.lstring);
+            });
+            Ext.TaskMgr.start(Application.MapTask);
         } else {
-          console.warn("Map component or map.events is not ready in doStuff");
+            console.warn("Map component or map.events is not ready in doStuff");
         }
 
-        (new Application.DebugWindow({
-            id: 'debug',
-            renderTo: Ext.getBody()
-        }));
+        new Application.DebugWindow({
+            id: "debug",
+            renderTo: Ext.getBody(),
+        });
 
-        (new Ext.Window({
-            id: 'loginwindow',
-            modal : true,
-            closable : false,
-            title : 'Weather.IM Live Login Options',
-            items : [createLoginPanel()]
-        })).show();
-    }
+        new Ext.Window({
+            id: "loginwindow",
+            modal: true,
+            closable: false,
+            title: "Weather.IM Live Login Options",
+            items: [createLoginPanel()],
+        }).show();
+    },
 });
 
-
 Application.MapLegend = Ext.extend(Ext.Window, {
-    width : 300,
-    height : 200,
-    autoScroll : true,
-    title : 'Map Legends',
-    hidden : true,
-    closeAction : 'hide',
+    width: 300,
+    height: 200,
+    autoScroll: true,
+    title: "Map Legends",
+    hidden: true,
+    closeAction: "hide",
 
-    initComponent : function() {
-        var config = {
-        };
+    initComponent: function () {
+        const config = {};
         Ext.apply(this, Ext.apply(this.initialConfig, config));
 
-        Application.MapLegend.superclass.initComponent.apply(this,
-                        arguments);
+        Application.MapLegend.superclass.initComponent.apply(this, arguments);
         this.buildItems();
     },
-    buildItems : function() {}
+    buildItems: function () {},
 });
